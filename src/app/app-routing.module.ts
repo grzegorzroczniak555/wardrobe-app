b631b785983d525fc2620d4b37b2c4b5ad4f3777
Rafal Bobko <rbobko70@gmail.com>
2020-03-26 15:47:25 -0400

    Revert "Feature/dashboard"

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,24 +1,9 @@
 import { HomePageComponent } from './home-page/home-page.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { DashboardComponent } from './dashboard/dashboard.component';
 
 const routes: Routes = [
-<<<<<<< HEAD
-  { path: 'main-page', component: MainPageComponent },
-  {
-    path: 'dashboard', children: [
-      { path: '', pathMatch: 'full', component: DashboardComponent },
-    ]
-  },
-  {
-    path: '',
-    redirectTo: 'main-page',
-    pathMatch: 'full'
-  }
-=======
   { path: 'home-page', component: HomePageComponent }
->>>>>>> develop
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,52 +5,23 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HomePageComponent } from './home-page/home-page.component';
 import { MatButtonModule } from '@angular/material/button';
-<<<<<<< HEAD
-import { MatSidenavModule } from '@angular/material/sidenav';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatListModule } from '@angular/material/list';
-import { MatIconModule } from '@angular/material/icon';
-import { AppRoutingModule } from './app-routing.module';
-import { DashboardComponent } from './dashboard/dashboard.component';
-import { LayoutModule } from '@angular/cdk/layout';
-import { NavbarComponent } from './navbar/navbar.component';
-import { MatExpansionModule } from '@angular/material/expansion';
-import { RouterModule } from '@angular/router';
-=======
 import { MatTooltipModule } from '@angular/material/tooltip';
 import { AppRoutingModule } from './app-routing.module';
 import { MainPageComponent } from './main-page/main-page.component';
->>>>>>> develop
 
 
 @NgModule({
   declarations: [
     AppComponent,
-<<<<<<< HEAD
-    MainPageComponent,
-    DashboardComponent,
-    NavbarComponent
-=======
     HomePageComponent,
     MainPageComponent
->>>>>>> develop
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatButtonModule,
-    MatSidenavModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatListModule,
     AppRoutingModule,
-<<<<<<< HEAD
-    LayoutModule,
-    MatExpansionModule,
-    RouterModule,
-=======
     MatTooltipModule
->>>>>>> develop
   ],
   providers: [],
   bootstrap: [AppComponent]
